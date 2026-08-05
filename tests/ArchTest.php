68c226b25c18990cdddb3639e5f25d1de7d0dfc1
Lutfi Sobri <lutfisobri@gmail.com>
2026-08-06 00:18:13 +0700

    test(arch): ignore tempnam in Pest Arch security preset

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 arch()->preset()->php()->ignoring(['debug_backtrace', 'tempnam', 'var_export']);
 
-arch()->preset()->security();
+arch()->preset()->security()->ignoring('tempnam');
 
 arch('it will not use dd(), ddd(), env(), or exit()')
     ->expect(['dd', 'ddd', 'env', 'exit'])
